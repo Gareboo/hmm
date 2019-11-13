641d9ccd9e7713d150379c6582fcc54e9c80575a
Glitch (b0d) <none>
2019-11-13 12:57:35 +0000

    📻🏬 Checkpoint ./commands/botara.js:6632949/349

--- commands/botara.js
+++ commands/botara.js
@@ -3,32 +3,34 @@ const db = require('quick.db')
 const request = require('request')
 
 exports.run = async (client, msg, args) => {
+  let mm = msg.mentions.members.first() || msg.guild.members.get(args[0])
+let ll = mm.id;
     let prefix = await db.fetch(`${msg.guild.id}.prefix`) || client.ayarlar.prefix
     if(!args[0]) {
       return msg.channel.send(new Discord.RichEmbed().setDescription('Please mention a bot' ).setColor("RED"))
     }
   if (db.has('botlar')) {
-  if (Object.keys(db.fetch('botlar')).includes(args[0]) === false) return msg.reply("This bot doesn't exist in api")
+  if (Object.keys(db.fetch('botlar')).includes(ll) === false) return msg.reply("This bot doesn't exist in api")
   };
-    request(`https://b0d.glitch.me/bot/${args[0]}`, function (error, response, body) {
+    request(`https://b0d.glitch.me/bot/${ll}`, function (error, response, body) {
     if (error) return msg.channel.send('Hata:', error);
     else if (!error) {
-      var a = db.fetch(`botlar.${args[0]}.isim`)
-      var b = db.fetch(`botlar.${args[0]}.id`)
-      var c = db.fetch(`botlar.${args[0]}.avatar`)
-      var d = db.fetch(`botlar.${args[0]}.prefix`)
-      var e = db.fetch(`botlar.${args[0]}.kutuphane`)
-      var g = db.fetch(`botlar.${args[0]}.kisaaciklama`)
-      var h = db.fetch(`botlar.${args[0]}.etiket`);
-      var owner = db.fetch(`botlar.${args[0]}.sahipid`)
-      var gh = db.fetch(`botlar.${args[0]}.durum`)
-      var gg = db.fetch(`botlar.${args[0]}.sertifika`)
+      var a = db.fetch(`botlar.${ll}.isim`)
+      var b = db.fetch(`botlar.${ll}.id`)
+      var c = db.fetch(`botlar.${ll}.avatar`)
+      var d = db.fetch(`botlar.${ll}.prefix`)
+      var e = db.fetch(`botlar.${ll}.kutuphane`)
+      var g = db.fetch(`botlar.${ll}.kisaaciklama`)
+      var h = db.fetch(`botlar.${ll}.etiket`);
+      var owner = db.fetch(`botlar.${ll}.sahipid`)
+      var gh = db.fetch(`botlar.${ll}.durum`)
+      var gg = db.fetch(`botlar.${ll}.sertifika`)
     }
       
       request(`https://b0d.glitch.me/bot`, function (error, response, body) {
     if (error) return msg.channel.send('Hata:', error);
     else if (!error) {
-    if (body.includes(args[0])=== true) return msg.reply("Bu ID'de bir bot sistemde bulunmamaktadır!")
+    if (body.includes(ll)=== true) return msg.reply("Bu ID'de bir bot sistemde bulunmamaktadır!")
     }
        })
       
